272921090ff025bbab6fd118407c1f76cb798f19
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-07 12:03:56 +0530

    Create Exercise-2.java

---
+++ Chapter 01/Exercises/Exercise-2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      String name = "Kapil";
+      String position = "Park Manager";
+      System.out.println("My name is " + name + " and I want to be a " + position + " in Mesozoic Eden.");
+    }
+}

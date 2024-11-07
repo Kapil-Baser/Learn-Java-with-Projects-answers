1f4ad2a12aa696cee4f9e8fc2f5ff45ff0d33d2c
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-07 12:09:47 +0530

    Create Exercise-3.java

---
+++ Chapter 01/Exercises/Exercise-3.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      String openingHours = "08:00";
+      String closingHours = "20:00";
+      System.out.println("Welcome to Mesozoic Eden. We are open from " + openingHours + " to " + closingHours);
+    }
+}

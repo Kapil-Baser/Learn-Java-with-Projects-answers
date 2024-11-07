629457530bd19479427a74e22527a609514f63c6
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-07 12:32:21 +0530

    Create Exercise-6.java

---
+++ Chapter 01/Exercises/Exercise-6.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      String name = "Kapil";
+      System.out.println("Welcome, " + name + " to Mesozoic Eden!");
+      System.out.println("Mesozoic Eden is safe and secure.");
+    }
+}

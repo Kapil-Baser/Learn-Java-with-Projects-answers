1e0aca2737d2d41f138537733bb5662cbaf8327c
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 10:21:46 +0530

    Create Exercise-7.java

---
+++ Chapter 02/Exercises/Exercise-7.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "magpie";
+        int age = 1;
+        boolean isCarnivore = true;
+        String diet = "Carnivore";
+        System.out.println("Name: " + name + " is " + age + " years old and their diet is: " + diet);
+    }
+}

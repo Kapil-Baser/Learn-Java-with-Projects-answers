c7e11d0d495c4a09d6d9cc4e1fcffe5ee287d496
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 10:08:57 +0530

    Create Exercise-3.java

---
+++ Chapter 02/Exercises/Exercise-3.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int maxVisitorsAllowed = 100;       
+        System.out.println("There is maximum of " + maxVisitorsAllowed + " people allowed in Mesozoic Eden.");
+    }
+}

074d63009734085ae9410d02f59fbe201293fbfd
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 10:33:39 +0530

    Create ProfileGenerator.java

---
+++ Chapter 02/Project/ProfileGenerator.java
@@ -0,0 +1,10 @@
+public class ProfileGenerator {
+    public static void main(String[] args) {
+        String name = "magpie";
+        int age = 1;
+        String species = "A. lithographica";
+        String diet = "Carnivore";
+        double weight = 0.95;
+        System.out.println("Meet " + name + ", a " + age + "-year-old " + species + ". As a " + diet + ", it has a robust weight of " + weight + " kilograms.");
+    }
+}

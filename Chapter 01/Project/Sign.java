57c0d68148b3ac002f1f63db19e421067a9c26d6
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 09:26:32 +0530

    Create Sign.java

---
+++ Chapter 01/Project/Sign.java
@@ -0,0 +1,10 @@
+public class Sign {
+    public static void main(String[] args) {
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+        String safetyMsg = "Mesozoic Eden is safe and secure.";
+        System.out.println("Welcome to Mesozoic Eden!");
+        System.out.println("Our opening and closing hours are from " + openingHours + " to " + closingHours);
+        System.out.println(safetyMsg);
+    }
+}

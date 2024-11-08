b2243265047e01076900a8242c13580db4287c2c
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 09:57:12 +0530

    Create Exercise-1.java

---
+++ Chapter 02/Exercises/Exercise-1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String breed = "Archaeopteryx";
+        double height = 0.3;
+        double length = 0.3;
+        double weight = 0.95;
+        System.out.println("Dinosaur breed: " + breed);
+        System.out.println("Height: " + height + " meters.");
+        System.out.println("Length: " + length + " meters.");
+        System.out.println("Weight: " + weight + " kg.");
+    }
+}

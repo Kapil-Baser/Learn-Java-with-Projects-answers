72a2797f1b35dd85b9e1de20d92d39c7e0ae3d49
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 10:11:24 +0530

    Create Exercise-4.java

---
+++ Chapter 02/Exercises/Exercise-4.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) { 
+        String name = "Ash";
+        int age = 25;
+        System.out.println("Employee's name: " + name);
+        System.out.println("Age: " + age + " years.");
+    }
+}

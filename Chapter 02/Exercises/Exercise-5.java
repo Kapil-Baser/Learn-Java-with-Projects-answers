d5da8b3f847d1d44b8d4a8311bad300e24efa641
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-08 10:14:27 +0530

    Create Exercise-5.java

---
+++ Chapter 02/Exercises/Exercise-5.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOfDinosaurs = 30;
+        System.out.println("Number of Dinosaur's in park: " + numberOfDinosaurs);
+    }
+}

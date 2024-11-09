0562428939cb36b6cf62efaecf76b94b031543e6
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-09 18:47:23 +0530

    Create Exercise-4.java

---
+++ Chapter 03/Exercises/Exercise-4.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int maxCapacity = 150;
+        int currentVisitors = 135;
+        boolean maxCapacityReached = currentVisitors >= maxCapacity;
+        System.out.println("Max capacity reached: " + maxCapacityReached);
+    }
+}

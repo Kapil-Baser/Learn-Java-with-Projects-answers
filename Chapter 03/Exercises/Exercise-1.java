dcaa6a13606bfb76acf99c597cf6c783ef290768
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-09 18:33:07 +0530

    Create Exercise-1.java

---
+++ Chapter 03/Exercises/Exercise-1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double dinosaurTimWeight = 5000;
+        double dinosaurJimWeight = 9000;
+        double average = (dinosaurTimWeight + dinosaurJimWeight) / 2;
+        System.out.println("Average weight of two dinosaurs: " + average + " kg.");
+    }
+}

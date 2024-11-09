ba9cc802d08aed2d6ecadc9fcaa5b2a0417f4079
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-09 18:51:07 +0530

    Create Exercise-5.java

---
+++ Chapter 03/Exercises/Exercise-5.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int JimsAge = 12;
+        int TimsAge = 22;
+        int ageDifference = Math.abs(JimsAge - TimsAge);
+        System.out.println("Age difference: " + ageDifference + " years.");
+    }
+}

1afd2f7490532fb0c9cb5af6c78e481ca55f89d7
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-10 11:14:35 +0530

    Create Exercise-1.java

---
+++ Chapter 04/Exercises/Exercise-1.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isCarnivore = true;
+        if (isCarnivore) {
+            System.out.println("Dinosaur is Carnivore.");
+        } else {
+            System.out.println("Dinosaur is Herbivore.");
+        }
+    }
+}

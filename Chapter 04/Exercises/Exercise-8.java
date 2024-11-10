85edaa7b57570c73b38ab5f6069ef43ed4715737
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-10 11:48:49 +0530

    Create Exercise-8.java

---
+++ Chapter 04/Exercises/Exercise-8.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentTime = 11;
+
+        if (currentTime >= 10 && currentTime <= 19) {
+            System.out.println("Park is open.");
+        } else {
+            System.out.println("Park is closed.");
+        }
+    }
+}

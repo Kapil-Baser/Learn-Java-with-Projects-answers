0acd6d63dff8f84a91c3d331d596470613d2c9b2
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-10 11:31:52 +0530

    Create Exercise-4.java

---
+++ Chapter 04/Exercises/Exercise-4.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int safetyThreshold = 8;
+        int currentRating = 10;
+
+        if (currentRating < safetyThreshold) {
+            System.out.println("Caution: Park is unsafe.");
+        } else {
+            System.out.println("Mesozoic Eden is safe and secure.");
+        }
+    }
+}

314a8aca3ffe9c9b70e8596c5847ef95b24551e7
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-10 11:28:26 +0530

    Create Exercise-3.java

---
+++ Chapter 04/Exercises/Exercise-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int requiredExperience = 12;
+        int currentExperience = 1;
+        
+        if (currentExperience >= requiredExperience) {
+            System.out.println("Enough experience to work.");
+        } else {
+            System.out.println("Not enough experience.");
+        }
+    }
+}

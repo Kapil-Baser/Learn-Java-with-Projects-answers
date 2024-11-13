5c5331263d716381708b8b9b08a159a9b2a18061
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-13 07:37:22 +0530

    Create Exercise-3.java

---
+++ Chapter 05/Exercises/Exercise-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int countdown = 30;       
+        while (countdown > 0) {
+            System.out.println("Park is opening in " + countdown + "seconds.");
+            countdown--;
+        }
+    }
+}

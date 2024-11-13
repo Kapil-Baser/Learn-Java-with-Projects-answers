f8b592867141bce7547ffc6094b9ba9f10a02cec
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-13 07:29:10 +0530

    Create Exercise-1.java

---
+++ Chapter 05/Exercises/Exercise-1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Printing all the ID's");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("dino" + i);
+        }
+    }
+}

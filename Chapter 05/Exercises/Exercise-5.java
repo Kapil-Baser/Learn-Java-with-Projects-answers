b71c77b08f0f46cc24673d9d3d463c65cc983651
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-13 07:44:55 +0530

    Create Exercise-5.java

---
+++ Chapter 05/Exercises/Exercise-5.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketsLeft = 50;
+        while (ticketsLeft != 0) {
+            System.out.println("Selling ticket.");
+            ticketsLeft--;
+        }
+        System.out.println("All tickets are sold out.");
+    }
+}

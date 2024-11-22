7dafba2bf08c7c515c298a375460415fe53ae0c6
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-22 09:55:01 +0530

    Create Exercise-4.java

---
+++ Chapter 06/Exercises/Exercise-4.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] employeeNames = {"Alice", "Bob", "Tim"};
+        for (String name : employeeNames) {
+            System.out.println(name + " works at Mesozoic Eden Park.");
+        }
+    }
+}

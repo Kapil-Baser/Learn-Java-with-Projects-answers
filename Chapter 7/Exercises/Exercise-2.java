7db25f4d3d878de72be7bc5e274e76338731c9e4
Kapil Kumar <151426701+Kapil-Baser@users.noreply.github.com>
2024-11-23 19:03:09 +0530

    Create Exercise-2.java

---
+++ Chapter 7/Exercises/Exercise-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double dinoWeight = 50;
+        double dailyFeed = getDailyFeed(50);
+        
+    }
+    public static double getDailyFeed(double weight) {
+        return weight * 0.5;
+    }
